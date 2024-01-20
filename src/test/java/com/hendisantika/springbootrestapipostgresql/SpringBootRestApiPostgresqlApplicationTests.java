0a3f0aba9a55347537b6896a3925f33db4158c1f
Adela Merko <merkoadela27@gmail.com>
2024-01-20 16:55:48 +0100

    Build

--- src/test/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplicationTests.java
+++ src/test/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplicationTests.java
@@ -1,13 +1,19 @@
 package com.hendisantika.springbootrestapipostgresql;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class SpringBootRestApiPostgresqlApplicationTests {
 
     @Test
     public void contextLoads() {
+        // Add assertions to check conditions within the loaded context
+        assertNotNull("Application context should not be null");
+        // Add more assertions as needed
     }
 
 }
